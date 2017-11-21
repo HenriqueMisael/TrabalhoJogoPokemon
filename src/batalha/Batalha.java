46b98e9f83c544e61bc88222cfb59d5a03d90962
HenriqueMisael <henrique.nolear@hotmail.com>
2017-11-21 00:26:17 -0200

    #2 - Implementação do pacote de Controle de Batalha *Adaptação da classe para servir melhor aos propósitos de testes, verificando se os times estão montados corretamente.

--- src/batalha/Batalha.java
+++ src/batalha/Batalha.java
@@ -1,8 +1,5 @@
 package batalha;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Batalha {
 
 	//ATRIBUTOS
@@ -11,12 +8,16 @@ public class Batalha {
     //CONSTRUTOR
     public Batalha(Jogador p1, Jogador p2) {
         time1 = p1;
-        time2 = p2;        
+        time2 = p2;      
+        
+        System.out.println("");
+        System.out.println(time1.toString());
+        System.out.println(time2.toString());
     }
     
     //METODOS
     public void start() {
-
+/*
         int vencedor = 0;
         Queue<Turno> turnos = new LinkedList<Turno>();
         
@@ -24,15 +25,15 @@ public class Batalha {
             
             turnos.add(new Turno(time1.escolherComando(), time2.escolherComando()));
             
-            /*
+            
                 Verifica vitória
-            */
+            
             if( !time1.temPokemonUtilizavel() )
                 vencedor = 1;
             else if( !time2.temPokemonUtilizavel() )
                 vencedor = 2;            
         }
         
-    }
+*/    }
 
 }
