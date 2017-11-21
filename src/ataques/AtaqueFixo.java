b39566b2335e9f342538f5eee1f21e9dcffdb3fd
HenriqueMisael <henrique.nolear@hotmail.com>
2017-11-21 12:36:10 -0200

    #4 - Implementação do pacote de Ataques +Implementação do esqueleto das classes AtaqueCharge, AtaqueFixo, AtaqueHp, AtaqueModifier, AtaqueMultiHit e AtaqueStatus.

---
+++ src/ataques/AtaqueFixo.java
@@ -0,0 +1,13 @@
+package ataques;
+
+public class AtaqueFixo extends Ataque {
+
+	private int val;
+	
+	@Override
+	public void efeito() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ataques/AtaqueStatus.java
@@ -0,0 +1,16 @@
+package ataques;
+
+import pokemon.Status;
+
+public class AtaqueStatus extends Ataque {
+
+	private Status status;
+	private int chance;	
+	
+	@Override
+	public void efeito() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
